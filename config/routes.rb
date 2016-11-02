107d855d0c75a7708ce9252fed761bc50123a7dd
Rick <rbciti@msn.com>
2016-11-02 13:05:10 -0700

    Add user and stock association and display

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  resources :user_stocks, except: [:show,  :update]
   #get 'welcome/index'
   root to: "welcome#index"
   get 'my_portfolio', to: "users#my_portfolio"
